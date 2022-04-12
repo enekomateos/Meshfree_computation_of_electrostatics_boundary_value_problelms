b3844d0aae1a0d1d4c66703140b98064fd0ee445
Juneirurzun <101326287+Juneirurzun@users.noreply.github.com>
2022-04-12 16:34:46 +0200

    Update paper_adibide.f90

--- paper_adibide/paper_adibide.f90
+++ paper_adibide/paper_adibide.f90
@@ -1,17 +1,34 @@
 module halton_sequence
-
 use mcf_tipos
+
 public :: halton
-
 contains
+function halton(b,n) result(r)
 
-   function halton(b)
-   
-       real(kind=dp)                 :: b
-       real(kind=dp), dimension(2,2) :: halton
-   
-   end function
+   integer, intent(in)   :: b
+   integer, intent(in)         :: n
+   real(kind=dp), dimension(n) :: r
+   real(kind=dp)               :: f,a
+   integer                     :: i,j
 
+   r(:)=0.0_dp
+
+   do j=1,n
+     i=j
+     f=1.0_dp
+     do
+        if (i>0) then
+           f=f/real(b,dp)
+           A=i-floor(i/real(b))*b
+           r(j)=r(j)+f*a
+           i=floor(i/real(b))
+        else
+           exit
+        end if
+      end do
+   end do
+
+end function halton
 end module halton_secuence
 
 program paper_adibidea
