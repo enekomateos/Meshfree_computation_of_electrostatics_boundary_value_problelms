b25b8ef3a1fdc6eff8ed26a8499d6bc300894044
Andoni <100809248+andonizaballa@users.noreply.github.com>
2022-04-12 14:27:00 +0200

    Update paper_adibide.f90

--- paper_adibide/paper_adibide.f90
+++ paper_adibide/paper_adibide.f90
@@ -1,5 +1,17 @@
 module halton_sequence
 
+use mcf_tipos
+public :: halton
+
+contains
+
+   function halton(b)
+   
+       real(kind=dp)                 :: b
+       real(kind=dp), dimension(2,2) :: halton
+   
+   end function
+
 end module halton_secuence
 
 program paper_adibidea
