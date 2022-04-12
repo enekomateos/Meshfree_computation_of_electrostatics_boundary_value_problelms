b665119f4f89e5fb793e752b28788a1ea1604a8a
enekomateos <100941413+enekomateos@users.noreply.github.com>
2022-04-12 18:45:45 +0200

    Halton sekuentzian azalpenak

--- paper_adibide/paper_adibide.f90
+++ paper_adibide/paper_adibide.f90
@@ -1,9 +1,11 @@
-module halton_sequence
-use mcf_tipos
+module halton_sequence                   ! Halton sekuentzia kalkulatzeko modulua, nodoak uniformeki sakabanatzeko
+ use mcf_tipos
 
-public :: halton
-contains
-function halton(b,n) result(r)
+ public :: halton
+
+ contains
+ 
+  function halton(b,n) result(r)         ! Funtzio honek b oinarriko halton sekuentziako lehenengo n balioak emango dizkigu bektore baten
 
    integer, intent(in)   :: b
    integer, intent(in)         :: n
@@ -11,12 +13,12 @@ function halton(b,n) result(r)
    real(kind=dp)               :: f,a
    integer                     :: i,j
 
-   r(:)=0.0_dp
+   r(:)=0.0_dp                           ! Bektorea inizializatu, 0 balioarekin indize bakoitzean
 
-   do j=1,n
+   do j=1,n                              ! Indize bakoitzerako sekuentziako balioa kalkulatu
      i=j
      f=1.0_dp
-     do
+     do                                  ! Do + if hau "while () do ()" egiteko da, eta ondoren algoritmoa aplikatzen da
         if (i>0) then
            f=f/real(b,dp)
            A=i-floor(i/real(b))*b
@@ -28,8 +30,9 @@ function halton(b,n) result(r)
       end do
    end do
 
-end function halton
-end module halton_secuence
+  end function halton
+ 
+ end module halton_secuence
 
 program paper_adibidea
  use mcf_tipos
