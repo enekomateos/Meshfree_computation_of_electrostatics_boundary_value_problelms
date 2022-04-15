15af61faaa50619fa79b3b94f6f146b4b1f4512e
enekomateos <100941413+enekomateos@users.noreply.github.com>
2022-04-15 19:58:22 +0200

    Update paper_adibide.f90
    dimentsioak sartu i,j-ri L funtzioan

--- paper_adibide/paper_adibide.f90
+++ paper_adibide/paper_adibide.f90
@@ -54,13 +54,14 @@ module halton_sequence                   ! Halton sekuentzia kalkulatzeko modulu
     
     
     function L_ij(phi,i,j,epsilon)
-     real(kind=dp), intent(in)        :: epsilon, i, j
-     real(kind=dp)                    :: L_ij, zatidura, fi
+     real(kind=dp), dimension(:) intent(in)        :: i,j
+     real(kind=dp), intent(in)                     :: epsilon
+     real(kind=dp)                                 :: L_ij, zatidura, fi
      interface
       function phi(i,j)
        use mcf_tipos
-        real(kind=dp), intent(in) :: i,j
-        real(kind=dp)             :: phi
+        real(kind=dp), dimension(:) intent(in) :: i,j
+        real(kind=dp)                          :: phi
        end function
      end interface
      
